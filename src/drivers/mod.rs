ec7a26bd49f8745d8af09bf3b475b8d1b50dff40
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-06 06:20:29 +1100

    Updates

---
+++ src/drivers/mod.rs
@@ -0,0 +1,9 @@
+// to control devices, like output, serial, usb, ssd
+
+struct Keyboard;
+
+struct Mouse;
+
+struct UART;
+
+struct Monitor;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("=================\nWITHIN SPECTRO VM\n=================");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        process_input();
+    }
 }
 
+fn process_input() {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer);
+    let res = buffer.trim();
 
+    if res == "exit" {
+        println!("exiting...");
+        std::process::exit(0);
+    }
+
+}
+
+// stuff to output to console
