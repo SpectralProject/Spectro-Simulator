d78a5c85edcf58aaae80ca42b7a48650c942246c
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-06 06:38:37 +1100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+use regex::Regex;
+use std::env;
 use std::io::{self, Write};
 
 fn main() {
     println!("=================\nWITHIN SPECTRO VM\n=================");
 
+    let _args: Vec<String> = env::args().collect();
+
+    // check if --bin exists and the arg that follows it
+    let elf_file = if let index = _args.iter().position(|r| r == "--bin").unwrap() {
+        // get index + 1
+        let _res = &_args[index + 1];
+        // check if there is a string that matches *.elf
+        if !_res.contains(".elf") {
+            std::process::exit(1);
+        }
+        // otherwise return the result
+        _res
+    } else {
+        // shouldnt happen
+        ""
+    };
+
+    // start the view server on another thread
+
+    // attempt to execute the elf file on another thread
+
+
+    // user interface 
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
@@ -20,7 +45,4 @@ fn process_input() {
         println!("exiting...");
         std::process::exit(0);
     }
-
 }
-
-// stuff to output to console
