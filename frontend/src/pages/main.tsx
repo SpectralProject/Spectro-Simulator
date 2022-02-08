5e1fcca97b7b5bc02ff1178bd4af8f7edbd5aa92
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-09 05:53:42 +1100

    Add skeletal code

---
+++ frontend/src/pages/main.tsx
@@ -0,0 +1,43 @@
+import { Text, Flex, Spacer, Box } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Main: NextPage = () => {
+    return (
+        <>
+            <Flex flexDir="column">
+                <Box w="100%" textAlign="center" p="1rem">
+                    <Text>Spectral BIOS v1</Text>
+                </Box>
+                <Flex flexDir="row" p="2rem">
+                    <Flex id="spectral-bios-sidebar" flexDir="column">
+                        <Text>SETTINGS</Text>
+                        <Text>OVERCLOCK</Text>
+                        <Text>UPGRADE BIOS</Text>
+                    </Flex>
+                    <Spacer />
+
+                    <Flex flexDir="column">
+                        <Text>Hardware</Text>
+                    </Flex>
+                    <Spacer />
+
+                    <Flex flexDir="column">
+                        <Text>Drives</Text>
+
+                    </Flex>
+                    <Spacer />
+
+                    <Flex flexDir="column">
+                        <Text>Peripherals</Text>
+                        {/* peripherals here */}
+                        <Text>USB</Text>
+                    </Flex>
+                </Flex>
+                {/* Could put a footer and disclaimer for the bios here */}
+            </Flex>
+
+        </>
+    )
+}
+
+export default Main
