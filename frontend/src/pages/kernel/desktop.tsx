e212c5ea9043d5be9cca8b5f892826f35097b554
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-03-21 12:50:19 +1100

    Works

--- frontend/src/pages/kernel/desktop.tsx
+++ frontend/src/pages/kernel/desktop.tsx
@@ -9,9 +9,15 @@ const DesktopEnv: NextPage = () => {
     const [blockScroll, allowScroll] = useScrollBlock()
     blockScroll()
 
+    const [contentOver, setContentOver] = React.useState(false)
+    const handleMouseOverContent = () => setContentOver(true)
+    const handleMouseOut = () => setContentOver(false)
+
+    // const handleContentDrag = () => false
+
     return (
         <>
-            <Flex flexDir="column" maxH="100vh" h="100vh" color="black">
+            <Flex flexDir="column" maxH="100vh" h="100vh" color="white" bgColor="grey">
                 <Box w="100%" textAlign="center" p="1rem" fontSize="3xl">
                     <Text>Welcome to ArcDesktop!</Text>
                 </Box>
@@ -24,7 +30,7 @@ const DesktopEnv: NextPage = () => {
                 </Box>
                 {/* PLACE WINDOW LOGIC HERE. ALSO WHERE BOTTOM DOCK LIVES USUALLY 
                 the header is the container row before the X on the right. The entire header is spaced [A A]*/}
-                <ArcWindow header={null} content={null} />
+                {/* <ArcWindow header={null} content={null} /> */}
                 {/* TODO: hook onto double click on the header and tell Rnd to resize */}
                 <Rnd
                     default={{
@@ -33,10 +39,20 @@ const DesktopEnv: NextPage = () => {
                         width: 320,
                         height: 200,
                     }}
+                    minWidth={300}
+                    minHeight={150}
                     enableUserSelectHack={false}
+                    disableDragging={contentOver}
                 >
-                    <Box bgColor="red.100" h="100%" w="100%">
-                        Rnd
+                    <Box bgColor="orange" cursor="default" h="100%" w="100%">
+                        <Box userSelect="none" className='header' pl="1rem" pt="0.5rem">
+                            Header
+                        </Box>
+                        {/* draggable="false" doesnt work */}
+                        {/* TODO: if trying to drag content, return; */}
+                        <Box cursor="default" color="black" className='content' onMouseEnter={handleMouseOverContent} onMouseLeave={handleMouseOut} bgColor="orange.50" p="0.5rem">
+                            Content
+                        </Box>
                     </Box>
                 </Rnd>
             </Flex>
